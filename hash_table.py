14b11567fee7bb498ca4886ec93a758b0b0e5213
Edward <edpark13@gmail.com>
2015-03-12 14:58:03 -0700

    create tests

--- hash_table.py
+++ hash_table.py
@@ -2,18 +2,22 @@
 
 
 class Hash_table(object):
+    """Implement a Hash and store a key and a value"""
 
     def __init__(self, size):
         self.size = size
         self.table = [[] for x in xrange(size)]
 
     def get_key(self, key):
+        """Given a key get the value associated with that key"""
         hash_key = self.hash(key)
         bucket = self.table[hash_key]
-        [answer for answer, v in enumerate(bucket) if v[0] == key]
-        return answer
+        for tup in bucket:
+            if tup[0] == key:
+                return tup[1]
 
     def set(self, key, value):
+        """Add a key and value pair into the Hash Table"""
         if type(key) == str:
             hashed_key = self.hash(key)
             self.table[hashed_key].append((key, value))
@@ -21,6 +25,7 @@ class Hash_table(object):
             raise TypeError('Keys must be strings')
 
     def hash(self, key):
+        """Given a key the hashed number of that key"""
         hash_value = 0
         for i in key:
             hash_value += ord(i)
