262f6ddb63099bb5449f00f33cf18a4694f60bfb
Edward <edpark13@gmail.com>
2015-03-19 13:57:29 -0700

    edit radixsort and wrote tests

--- radixsort.py
+++ radixsort.py
@@ -1,21 +1,24 @@
 #! /usr/bin/env python
 import math
+import random
 
 def radixsort(l):
-    digits = int(math.log10(max(l))+1)
-    for i in xrange(1, digits+1):
-        a = [[] for x in xrange(0, 10)]
-        for x in l:
-            a[(x%10^i)//(10^(i-1))].append(x)
-        l = []
-        for i in xrange(10):
-            l.extend(a[i])
-    return l
+    try:
+        digits = int(math.log10(max(l))+1)
+        for i in xrange(1,digits+1):
+            ll = [[] for p in xrange(0,10)]
+            # import pdb; pdb.set_trace()
+            for x in l:
+                ll[x%10**i//(10**(i-1))].append(x)
+            l = []
+            for j in xrange(10):
+                l.extend(ll[j])
+        return l
+    except ValueError:
+        return []
 
 
 if __name__ == '__main__':
-    import pdb
-    test = [3, 5, 289, 35, 90, 113]
-    # pdb.set_trace()
-    print radixsort(test)
-    print sorted(test)
+    randoml = random.sample(range(100), 10)
+    print radixsort(randoml)
+    print sorted(randoml)
---
+++ test_radixsort.py
@@ -0,0 +1,29 @@
+import pytest
+import random
+from radixsort import radixsort
+
+def test_empty(empty_list):
+    assert radixsort(empty_list) == []
+
+def test_sort(sorted_list):
+    assert radixsort(sorted_list) == sorted_list
+
+def test_rand(made_list):
+    assert radixsort(made_list) == sorted(made_list)
+
+
+@pytest.fixture(scope='function')
+def made_list():
+    made_list = random.sample(range(1000), 100)
+    print made_list
+    return made_list
+
+@pytest.fixture(scope='function')
+def sorted_list():
+    return range(10)
+
+@pytest.fixture(scope='function')
+def empty_list():
+    l = []
+    return l
+
