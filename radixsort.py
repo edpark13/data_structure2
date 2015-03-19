874f9f8a0d5981583a9d5d0f2cf72573001164a9
efrainc <efrainc88@gmail.com>
2015-03-19 13:26:12 -0700

    Started Radixsort

--- radixsort.py
+++ radixsort.py
@@ -1,1 +1,21 @@
+#! /usr/bin/env python
+import math
+
 def radixsort(l):
+    digits = int(math.log10(max(l))+1)
+    for i in xrange(1, digits+1):
+        a = [[] for x in xrange(0, 10)]
+        for x in l:
+            a[(x%10^i)//(10^(i-1))].append(x)
+        l = []
+        for i in xrange(10):
+            l.extend(a[i])
+    return l
+
+
+if __name__ == '__main__':
+    import pdb
+    test = [3, 5, 289, 35, 90, 113]
+    # pdb.set_trace()
+    print radixsort(test)
+    print sorted(test)
